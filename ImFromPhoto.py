249368a310333782c198f2f4cf605b062d01bc05
siggo <sig.goldbach@gmail.com>
2023-03-15 12:59:11 +0100

    New robot pictures with new color

--- ImFromPhoto.py
+++ ImFromPhoto.py
@@ -5,11 +5,14 @@ import cv2 as cv
 import numpy as np
 import time
 
-image = cv.imread('Resources/Pictures/fieldWithBalls.jpg')
+image = cv.imread('Resources/Pictures/withRobot.jpg')
 print(image[0][0])
 # cv.imshow('Read picture', image)
 img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 blank = np.zeros((480, 640, 3), dtype='uint8')
+blank2 = np.zeros((480, 640, 3), dtype='uint8')
+
 print(image[0][0][2])
 
 lower_red = np.array([170, 50, 180])
@@ -22,19 +25,26 @@ img_height, img_width, _ = image.shape
 red_pixels = 0
 for i in range(img_height):
     for j in range(img_width):
-        if image[i][j][2] >= 180 and image[i][j][2] <= 255:
+        # detecting walls
+        if 155 <= image[i][j][2] <= 255 and 0 <= image[i][j][0] <= 100 and 0 <= image[i][j][1] <= 100:
             red_pixels += 1
             blank[i][j][2] = 255
-            if image[i][j][0] > 180 and image[i][j][1] > 180:
-                blank[i][j][0] = 255
-                blank[i][j][1] = 255
+        if 180 <= image[i][j][0] and 180 <= image[i][j][1] and 180 <= image[i][j][2]:
+            blank[i][j][0] = 255
+            blank[i][j][1] = 255
+            blank[i][j][2] = 255
 
 end = time.time()
+for i in range(img_height):
+    for j in range(img_width):
+        if 30 <= gray[i][j] <= 70:
+            blank2[i][j][2] = 255
 
 timeForTransform = end - start
 print('time for transform: ' + str(timeForTransform))
 print("Amount of red pixels: " + str(red_pixels))
-cv.imshow('withRed', blank)
+cv.imshow('Original', image)
+cv.imshow('BetterModel', blank)
 
 # cv.imshow('StartModel', blank)
 cv.waitKey(0)
