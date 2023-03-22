7edb08a3a0690902d1e64e5ccfb0c850fda75cde
seburu <sebynoah@gmail.com>
2023-03-22 10:03:46 +0100

    No relevant changes just chaning pc

--- ImFromPhoto.py
+++ ImFromPhoto.py
@@ -5,13 +5,21 @@ import cv2 as cv
 import numpy as np
 import time
 
-image = cv.imread('Resources/Pictures/withRobot.jpg')
+
+class Field:
+    width = 120
+    height = 180
+    ball_location = list
+    ball_amount = 0
+
+
+image = cv.imread('Resources/Pictures/fieldWithBalls.jpg')
 print(image[0][0])
 # cv.imshow('Read picture', image)
 img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 blank = np.zeros((480, 640, 3), dtype='uint8')
-blank2 = np.zeros((480, 640, 3), dtype='uint8')
+# blank2 = np.zeros((480, 640, 3), dtype='uint8')
 
 print(image[0][0][2])
 
@@ -23,6 +31,8 @@ i_counter = 0
 j_counter = 0
 img_height, img_width, _ = image.shape
 red_pixels = 0
+maxVal = list
+
 for i in range(img_height):
     for j in range(img_width):
         # detecting walls
@@ -33,12 +43,9 @@ for i in range(img_height):
             blank[i][j][0] = 255
             blank[i][j][1] = 255
             blank[i][j][2] = 255
+# Here I will try to find the four corners by finding extreme red values
 
 end = time.time()
-for i in range(img_height):
-    for j in range(img_width):
-        if 30 <= gray[i][j] <= 70:
-            blank2[i][j][2] = 255
 
 timeForTransform = end - start
 print('time for transform: ' + str(timeForTransform))
