32180d46593d7d7327ab8f4031502dd274627fba
Christian H <christian@hyltoft.net>
2023-06-01 09:41:52 +0200

    Better edgde detection and cleanup from loops

--- ImFromPhoto.py
+++ ImFromPhoto.py
@@ -1,10 +1,6 @@
-# Sebastian Goldbach
-# This file is made with the purpose of taking a picture and mapping what is currently in it
-
 import cv2 as cv
 import numpy as np
 import time
-
 
 class Field:
     width = 120
@@ -12,31 +8,27 @@ class Field:
     ball_location = list
     ball_amount = 0
 
-
-image = cv.imread('Resources/Pictures/fieldWithBalls.jpg')
+image = cv.imread('Resources/Pictures/fieldWithBalls_concealed.jpg')
 print(image[0][0])
-# cv.imshow('Read picture', image)
-img_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-blank = np.zeros((480, 640, 3), dtype='uint8')
-# blank2 = np.zeros((480, 640, 3), dtype='uint8')
 
-print(image[0][0][2])
+# Convert the image to grayscale
+gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+# Create a blank image
+blank = np.zeros_like(image)
 
 lower_red = np.array([170, 50, 180])
 upper_red = np.array([180, 255, 255])
-# trying to change blank to a better model
+
 start = time.time()
-i_counter = 0
-j_counter = 0
-img_height, img_width, _ = image.shape
+
 red_pixels = 0
 white_pixels = 0
-maxVal = list
+
+img_height, img_width, _ = image.shape
 
 for i in range(img_height):
     for j in range(img_width):
-        # detecting walls
         if 155 <= image[i][j][2] <= 255 and 0 <= image[i][j][0] <= 100 and 0 <= image[i][j][1] <= 100:
             red_pixels += 1
             blank[i][j][2] = 255
@@ -45,11 +37,13 @@ for i in range(img_height):
             blank[i][j][0] = 255
             blank[i][j][1] = 255
             blank[i][j][2] = 255
-# Here I will try to find the four corners by finding extreme red values
+
 end = time.time()
+
 im_gray = cv.cvtColor(blank, cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(im_gray, 127, 255, 0)
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
 circles2 = 0
 circles_list = []
 for cnt in contours:
@@ -60,41 +54,34 @@ for cnt in contours:
         M = cv.moments(cnt)
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
-        print("Center of this circle should be: " + str(cX) + " " + str(cY))
+        print("Center of this circle should be:", cX, cY)
         cv.circle(blank, (cX, cY), 7, (255, 0, 0), -1)
         circles_list.append([(cX, cY)])
-# Now to find the big square
+
+# Adjust the parameters for the HoughLinesP function to improve the line detection
 edges = cv.Canny(im_gray, 50, 150, apertureSize=3)
 lines = cv.HoughLinesP(
-    edges,  # Input edge image
-    1,  # Distance resolution in pixels
-    np.pi / 180,  # Angle resolution in radians
-    threshold=100,  # Min number of votes for valid line
-    minLineLength=5,  # Min allowed length of line
-    maxLineGap=10  # Max allowed gap between line for joining them
+    edges,
+    rho=1,
+    theta=np.pi / 180,
+    threshold=50,  # Adjust this value for line detection sensitivity
+    minLineLength=100,  # Adjust this value for the minimum length of lines
+    maxLineGap=10  # Adjust this value for the maximum gap between line segments
 )
 
 lines_list = []
-for points in lines:
-    # Extracted points nested in the list
-    x1, y1, x2, y2 = points[0]
-    # Draw the lines joing the points
-    # On the original image
-    cv.line(blank, (x1, y1), (x2, y2), (0, 255, 0), 2)
-    # Maintain a simples lookup list for points
-    lines_list.append([(x1, y1), (x2, y2)])
-
-print("Contour length: " + str(len(contours)))
-print("Line amount: " + str(len(lines_list)))
+if lines is not None:
+    for points in lines:
+        x1, y1, x2, y2 = points[0]
+        cv.line(blank, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        lines_list.append([(x1, y1), (x2, y2)])
 
 timeForTransform = end - start
-print('time for transform: ' + str(timeForTransform))
-print("Amount of red pixels: " + str(red_pixels))
-print("Amount of white pixels: " + str(white_pixels))
-print("Amount of circles: " + str(circles2))
+print('Time for transform:', timeForTransform)
+print("Amount of red pixels:", red_pixels)
+print("Amount of white pixels:", white_pixels)
+print("Amount of circles:", circles2)
 
 cv.imshow('Original', image)
-cv.imshow('Obstacles and balls drawn: ', blank)
-
-# cv.imshow('StartModel', blank)
+cv.imshow('Obstacles and balls drawn:', blank)
 cv.waitKey(0)
