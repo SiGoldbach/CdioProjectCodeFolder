264b8efea1b20a56da36d039ae0bfa6106eaf13c
siggo <sig.goldbach@gmail.com>
2023-06-01 16:00:01 +0200

    Main staged

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from threading import Thread
 import sys
-
+import subprocess
+import json
 # from robotInfo import GameInfo
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -16,7 +17,7 @@ import sys
 
 
 import socket
-
+import time
 # Create your objects here.
 ev3 = EV3Brick()
 left_wheel_motor = Motor(Port.A)
@@ -25,31 +26,13 @@ convey_motor = Motor(Port.C)
 ramp_motor = Motor(Port.D)
 
 
+
+
+
+
 # method to make both wheels drive.
 # convey_motor.run_angle(700,30000,wait=False)
 # robot = DriveBase(left_wheel_motor, right_wheel_motor, wheel_diameter=55.5, axle_track=104)
-
-def main():
-    next_move = MoveFinder.find_move()
-    match next_move.my_type:
-        case commandEnum.MoveType.RIGHT:
-            robotTurnRight(next_move.speed, next_move.argument)
-        case commandEnum.MoveType.LEFT:
-            robotTurnLeft(next_move.speed, next_move.argument)
-        case commandEnum.MoveType.START_CONVEYER:
-            collectBalls(next_move.argument)
-        case commandEnum.MoveType.DELIVER:
-            openCloseHatch()
-        case commandEnum.MoveType.BACKWARD:
-            moveBackward(next_move.speed, next_move.argument)
-        case commandEnum.MoveType.FORWARD:
-            moveForward(next_move.speed, next_move.argument)
-        case commandEnum.MoveType.UNSTUCK:
-            unstuckBall()
-
-    print("The game has started")
-
-
 # robotTurnRight(200, 175, left_wheel_motor, right_wheel_motor) is equal to a 90 degrees right turn
 #def robotTurnRight(speed, ang):
  #   left_wheel_motor.run_angle(speed, ang, wait=False)
@@ -93,17 +76,26 @@ def unstuckBall():
     convey_motor.run_target(200, -1000)
 
 # Write your program here.
+
 ev3.speaker.beep()
+for i in range(10):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("10.209.192.170", 5000))
+    sock.send(b"GET /test HTTP/1.1\r\nHost:10.209.192.170\r\n\r\n")
+    response = sock.recv(4096)
+    print(response.decode())
+    moveForward(500, 200)
+    time.sleep(1)
+    sock.close()
 
 # Start the program
-collectBalls(10000)
+# collectBalls(10000)
 # moveBackward(600, 200)
 # turnRight(200, 175)
-moveForward(600, 3000)
 # turnLeft(200, 800)
-turn(200, 800)
-moveForward(600, 2500)
+# turn(200, 800)
+# moveForward(600, 2500)
 # turnRight(200, 750)
-turn(200, -750)
-moveBackward(600, 500)
-openCloseHatch()
+# turn(200, -750)
+# moveBackward(600, 500)
+# openCloseHatch()
