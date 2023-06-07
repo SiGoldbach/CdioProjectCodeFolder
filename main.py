8354ab78058bbd15d0576e1fed83614d250aa6a8
siggo <sig.goldbach@gmail.com>
2023-06-07 12:58:02 +0200

    New test With gyro

--- main.py
+++ main.py
@@ -25,8 +25,15 @@ import time
 ev3 = EV3Brick()
 left_wheel_motor = Motor(Port.A)
 right_wheel_motor = Motor(Port.B)
-convey_motor = Motor(Port.C)
-ramp_motor = Motor(Port.D)
+convey_motor = Motor(Port.D)
+ramp_motor = Motor(Port.C)
+gyro=GyroSensor(Port.S2)
+robot = DriveBase(left_wheel_motor,right_wheel_motor,35,110)
+print("Calibarting gyro")
+wait(5000)
+
+#I am here making a motor pair 
+
 
 
 
@@ -47,8 +54,8 @@ ramp_motor = Motor(Port.D)
   #  right_wheel_motor.run_angle(speed, ang)
 
 def turn(speed, ang):
-        left_wheel_motor.run_angle(speed, -ang, wait=False)
-        right_wheel_motor.run_angle(speed, ang)
+        left_wheel_motor.run_angle(speed, ang, wait=False)
+        right_wheel_motor.run_angle(speed, -ang)
 
 
 # method to move forwards
@@ -65,18 +72,18 @@ def moveBackward(speed, dist):
 
 # make ramp open and clsoe at good speed and angle
 def openCloseHatch():
-    ramp_motor.run_target(300, 100)
+    ramp_motor.run_target(300, -40)
     print("Openened the ramp")
     wait(2000)
     print("About to close the ramp")
-    ramp_motor.run_target(250, -20)
+    ramp_motor.run_target(250, 40)
     print("DEBUGGIN")
     print("done using the latch")
 
 # collecting the balls at a good speed for a given time
 def collectBalls(time):
     print("Collecting Balls")
-    convey_motor.run_target(700, time, wait=False)
+    convey_motor.run_angle(700, -time, wait=False)
     print("Done collecting Balls")
 
 # remove stuck balls if there is any
@@ -94,42 +101,53 @@ def takeMove(move):
         turn(move.speed,move.argument)
     if move.type==moveOptions.DELIVER:
         openCloseHatch()
-    
-    
-    
 
+def angleMove():
+    print(gyro.angle())
+    robot.drive(0, 180)
+    while gyro.angle()<90:
+        wait(10)
+    robot.stop
+    print(gyro.angle())
+
+    
+    
+    
 # Write your program here.
 #I am here starting a thread to take care of the conveyor
-pick_balls_thread=threading.Thread(target = collectBalls(500000))
-pick_balls_thread.start
 
-# ev3.speaker.beep()
-for i in range(400):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("10.209.192.170", 5000))
-    sock.send(b"GET /test HTTP/1.1\r\nHost:10.209.192.170\r\n\r\n")
-    response = ''
-    while True:
-        data = sock.recv(1024)
-        if not data:
-            break
-        response += data.decode('utf-8')
-    headers, body = response.split('\r\n\r\n', 1)
-    move = json.loads(body, object_hook=MoveFinder.as_payload)
-    print("Given move is: "+move.toString())
-    takeMove(move)
-    time.sleep(1)
-    sock.close()
+#Here the current competition mode for the robot is written 
 
     
-# Start the program
-# collectBalls(10000)
-# moveBackward(600, 200)
-# turnRight(200, 175)
-# turnLeft(200, 800)
-# turn(200, 800)
-# moveForward(600, 2500)
-# turnRight(200, 750)
-# turn(200, -750)
-# moveBackward(600, 500)
-# openCloseHatch()
+#Here is a function written to test the gyro 
+def gyroTest():
+    print(gyro.angle())
+    #angleMove()
+    #print(gyro.angle())
+    #print("Should be close to 0: ",gyro.speed())
+    
+angleMove()
+
+
+
+def comp():
+    pick_balls_thread=threading.Thread(target = collectBalls(500000))
+    pick_balls_thread.start
+   # for i in range(2):
+   # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   # sock.connect(("10.209.192.170", 5000))
+   # sock.send(b"GET /test HTTP/1.1\r\nHost:10.209.192.170\r\n\r\n")
+   # response = ''
+   # while True:
+   #     data = sock.recv(1024)
+   #     if not data:
+   #         break
+   #     response += data.decode('utf-8')
+   # headers, body = response.split('\r\n\r\n', 1)
+   # move = json.loads(body, object_hook=MoveFinder.as_payload)
+   # print("Given move is: "+move.toString())
+   # takeMove(move)
+   # time.sleep(1)
+   # sock.close()
+    
+
